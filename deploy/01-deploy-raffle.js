a00099520a8dbeeff24371b003a9fe2b0c076068
AjayAsnani <101942594+AjayAsnani@users.noreply.github.com>
2022-08-09 23:06:37 +0530

    fixed

--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -1,5 +1,9 @@
 const { network, ethers } = require("hardhat")
-const { networkConfig, developmentChains } = require("../helper-hardhat-config")
+const {
+    networkConfig,
+    developmentChains,
+    VERIFICATION_BLOCK_CONFIRMATIONS,
+} = require("../helper-hardhat-config")
 
 const { verify } = require("../utils/verify")
 
